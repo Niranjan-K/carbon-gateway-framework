09b37382b96c16d947d0999e71da16c6ef1d06d3
RaivU <ravi.undupitiya@gmail.com>
2016-05-26 08:45:45 +0530

    Pipeline communication and URI/API Templates

---
+++ gateway-core/components/org.wso2.carbon.gateway.core/src/main/java/org/wso2/carbon/gateway/core/config/dsl/external/wuml/Environment.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.gateway.core.config.dsl.external.wuml;
+
+/**
+ * Created by ravi on 5/24/16.
+ */
+public class Environment {
+}
---
+++ gateway-core/components/org.wso2.carbon.gateway.core/src/main/java/org/wso2/carbon/gateway/core/flow/mediators/builtin/flowcontrollers/pipeline/PipelineMediator.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.gateway.core.flow.mediators.builtin.flowcontrollers.pipeline;
+
+/**
+ * Created by ravi on 5/24/16.
+ */
+public class PipelineMediator {
+}
